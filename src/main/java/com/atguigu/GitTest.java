5ddaf165d602f16ac14f7d785344184bda44cad2
cs <2065952961@qq.com>
2022-07-06 03:16:38 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world4");
+
 
 
     }
